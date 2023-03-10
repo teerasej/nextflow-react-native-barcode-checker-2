1b5a591c70dec86bf3584fe355da3f5168457603
teerasej <teerasej@gmail.com>
2023-03-10 23:12:12 +0700

    Load barcode data from redux to show in home page

--- pages/home-page/HomePage.js
+++ pages/home-page/HomePage.js
@@ -1,12 +1,24 @@
 import { StyleSheet, View } from 'react-native'
-import React from 'react'
+import React, { useEffect } from 'react'
 // import component ที่จำเป็น
 import { Box, HStack, Text } from 'native-base'
+import { useSelector } from 'react-redux'
+import { useIsFocused } from '@react-navigation/native'
 
 const HomePage = () => {
+
+    let barcodeData = useSelector(state => state.barcode.barcodeData)
+
+    console.log(barcodeData);
+
     return (
         <>
-            
+            <Box padding="10">
+                {barcodeData ?
+                    <Text>Barcode Data: {barcodeData}</Text>
+                    : <Text>Nothing here...</Text>
+                }
+            </Box>
         </>
     )
 }
