abcfd966ffb548d7b2b21a7b93d807ab56c547ad
teerasej <teerasej@gmail.com>
2023-03-10 17:06:26 +0700

    Create scan page

---
+++ pages/scan-page/ScanPage.js
@@ -0,0 +1,16 @@
+import { StyleSheet, View } from 'react-native'
+import React from 'react'
+// import component ที่จำเป็น
+import { Box, HStack, Text } from 'native-base'
+
+const ScanPage = () => {
+    return (
+        <>
+            
+        </>
+    )
+}
+
+export default ScanPage
+
+const styles = StyleSheet.create({})
