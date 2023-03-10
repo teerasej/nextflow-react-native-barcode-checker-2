5a9cc1e982529a5d59b2043f0132ad32bd8c85bc
teerasej <teerasej@gmail.com>
2023-03-10 17:15:26 +0700

    Remove due to using React Navigation's header instead

--- pages/home-page/HomePage.js
+++ pages/home-page/HomePage.js
@@ -6,15 +6,7 @@ import { Box, HStack, Text } from 'native-base'
 const HomePage = () => {
     return (
         <>
-            {/* กำหนดพื้นที่ safe area และสี */}
-            <Box safeAreaTop bgColor="violet.800" />
-            {/* กำหนดส่วนที่เป็น  header */}
-            <HStack bg="violet.800" px="1" py="3" justifyContent="space-between" w="100%">
-                {/* กำหนดข้อความ */}
-                <Text color="white" fontSize="20" fontWeight="bold">
-                    Home
-                </Text>
-            </HStack>
+            
         </>
     )
 }
