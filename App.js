f5967e33e3f7c286ebc37bf965712e16a41b4107
teerasej <teerasej@gmail.com>
2023-03-10 17:56:10 +0700

    navigate to scan page

--- App.js
+++ App.js
@@ -1,19 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
-import { NativeBaseProvider, Box } from "native-base";
+import { NativeBaseProvider, Box, IconButton, Icon } from "native-base";
 import HomePage from './pages/home-page/HomePage';
-
+import { FontAwesome } from '@expo/vector-icons';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import ScanPage from './pages/scan-page/ScanPage';
+
 const Stack = createNativeStackNavigator();
+
+
 
 export default function App() {
   return (
     <NativeBaseProvider>
       <NavigationContainer>
         <Stack.Navigator>
-          <Stack.Screen name="Home" component={HomePage} options={{ title: 'My home' }} />
+          <Stack.Screen name="Home" 
+          component={HomePage} 
+          options={({ navigation }) => ({ 
+            title: 'My home',
+            headerRight: () => (
+              <IconButton 
+                icon={<Icon as={FontAwesome} name="qrcode"/>}
+                borderRadius="full"
+                onPress={() => navigation.navigate('Scan')}
+              />
+            ) 
+          })} 
+          />
 
           {/* กำหนด Scan Page เป็นอีก 1 Screen พร้อมทั้ง name และ component  */}
           <Stack.Screen name="Scan" component={ScanPage} />
