258d17f6e873f4eaf71df587fdfbed43a3892191
teerasej <teerasej@gmail.com>
2023-03-11 22:35:21 +0700

    Use barcode histories in home page

--- pages/home-page/HomePage.js
+++ pages/home-page/HomePage.js
@@ -1,24 +1,48 @@
 import { StyleSheet, View } from 'react-native'
 import React, { useEffect } from 'react'
 // import component ที่จำเป็น
-import { Box, HStack, Text } from 'native-base'
+import { Box, FlatList, HStack, Text, VStack } from 'native-base'
 import { useSelector } from 'react-redux'
-import { useIsFocused } from '@react-navigation/native'
 
 const HomePage = () => {
 
-    let barcodeData = useSelector(state => state.barcode.barcodeData)
+    // ยกเลิกการดึง barcode.barcodeData 
+    // let barcodeData = useSelector(state => state.barcode.barcodeData)
 
-    console.log(barcodeData);
+    // ใช้ข้อมูลจาก barcode.barcodeHistories ในการแสดงรายการข้อมูลย้อนหลังแทน
+    const barcodeHistories = useSelector(state => state.barcode.barcodeHistories)
 
+    // แสดงข้อมูลใน barcodeHistories บน console
+    console.log(barcodeHistories);
+
+    // ปรับการแสดงข้อมูลบนหน้า home page ให้ตอบรับกับข้อมูลการแสกนย้อนหลัง
     return (
         <>
-            <Box padding="10">
-                {barcodeData ?
-                    <Text>Barcode Data: {barcodeData}</Text>
-                    : <Text>Nothing here...</Text>
-                }
-            </Box>
+            {
+                // เช็คว่ามีข้อมูลย้อนหลังของ barcode หรือไม่ ถ้ามีให้แสดงเป็น list
+                barcodeHistories.length > 0 ? (
+                    <FlatList
+                    // กำหนด barcodeHistories เป็นข้อมูลของ FlatList
+                        data={barcodeHistories}
+                    // กำหนด function render ที่จะ return เป็น component ที่ต้องการแสดงข้อมูลของแต่ละ item ที่อยู่ใน data ของ FlatList
+                    // ในที่นี้ function จะได้ object item มาใช้งานด้วย
+                        renderItem={({ item }) => (
+                            <Box borderBottomWidth="1" paddingLeft={5} paddingBottom={5} paddingTop={5}>
+                                <VStack space={1}>
+                                    <Text>id: {item.id}</Text>
+                                    <Text>code: {item.barcodeData}</Text>
+                                </VStack>
+                            </Box>
+                        )}
+                    />
+                    // ถ้าไม่มีให้แสดงเป็นข้อความ nothing here...
+                ) : (
+                    <Box padding="10">
+                        <Text>Nothing here...</Text>
+                    </Box >
+                )
+            }
+
         </>
     )
 }
